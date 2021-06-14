25c42e3dd2142e10f0f67d28c082d05e1de38835
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-06-13 23:49:49 -0300

    Configure i18n and TimeZones

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,20 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_locale_configs_and_timezone
+  config.time_zone = 'Brasilia'
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locale', '**/*.{rb, yml}').to_s]
+  config.i18n.available_locales = ['pt-BR']
+  config.i18n.default_locale = 'pt-BR'
+end
+
+
 module OpenVagas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    set_locale_configs_and_timezone
 
     # Configuration for the application, engines, and railties goes here.
     #
